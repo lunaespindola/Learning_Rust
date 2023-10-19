59922c92723780fb21ded9feb2998f532024609d
Luna <A01751117@tec.mx>
2023-10-18 21:55:30 -0600

    Finished

--- omega_up/src/main.rs
+++ omega_up/src/main.rs
@@ -1,5 +1,4 @@
 // A. El mayor de dos
-// use std::io::{Read, self};
 // fn main() {
 //     let mut input = String::new();
 //     while io::stdin().read_line(&mut input).unwrap() > 0 {
@@ -53,51 +52,22 @@
 // }
 
 // C. Los Feriapesos
-/*
-El premio que recibió Norman por ganar el juego de tiro al pato fue un fajo de Feriapesos, 
-los cuales son billetes que pueden utilizarse para pagar o reducir el precio de diversos 
-alimentos en la sección de comida. Cada establecimiento que acepta Feriapesos los cambia siempre a razón de 10 a 1, es decir, 
-10 Feriapesos equivale a un peso real y sólo se podrán cambiar en múltiplos de 10, 
-con la finalidad de evitar los centavos. Por ejemplo, 15 Feriapesos equivalen solo a 1 peso, sin embargo 
-20 Feriapesos se cambian por 2 pesos.
-
-A Norman se le antojó un buñuelo que cuesta x pesos y quiere utilizar la mayor cantidad de Feriapesos para pagarlo, 
-sin embargo, debido a la restricción en el cambio de los Feriapesos, no sabe bien cuál es la cantidad que aún debe pagar. 
-¿Podrías de nueva cuenta ayudar a Norman?
-
-Entrada
-La primera línea se compone de un entero  X que representa el precio del buñuelo y la siguiente contiene un entero 
-F que simboliza la cantidad de Feriapesos que Norman posee. Suponer que 0 <= X, F<=10^6
-
-Salida
-Imprimir el símbolo $ seguido de un entero que representa la cantidad de dinero que aún debe pagar Norman para recibir su buñuelo.
-
-Ejemplo de entrada
-8
-29
-
-Ejemplo de salida
-$6
-
-Ejemplo de entrada 2
-1
-10
-
-Ejemplo de salida 2
-$0
-*/
 // use std::io::Read;
-
-// fn main() {
+// fn main()  {
 //     let mut buffer = String::new();
 //     std::io::stdin().read_to_string(&mut buffer).unwrap();
-//     let mut input = buffer.split_ascii_whitespace();
+//     let mut input = buffer.split_whitespace();
 
-//     let x: u32 = input.next().unwrap().parse().unwrap();
-//     let f: u32 = input.next().unwrap().parse().unwrap();
+//     let x: i32 = input.next().unwrap().parse().unwrap();
+//     let f: i32 = input.next().unwrap().parse().unwrap();
 
 
-
+//     if (f/10) >= x {
+//         print!("$0")
+//     } 
+//     else {
+//         println!("${}", x - (f / 10));
+//     }
 // }
 
 // D. Imprimiendo enteros por paridad
@@ -218,83 +188,61 @@ $0
 // }
 
 // I. Alcoholic Pilots
-/*
-In one of his many trips, Mr. Ed boarded an airplane where the captain talked like… well, like he was completely drunk. “I would like to greet a very special person here, which has been part of our lives for so much time. His wife says from the control tower that she loves you” – the captain said. Of course, Mr. Ed was really scared, how can alcoholic pilots flight with so many people on their hands? But that was not the worst part, our friend noticed that these drunken pilots like to race between them!
-By getting close to the captain’s cabin, Mr. Ed could hear another pilot (drunk, as expected) discussing with the captain by radio. Both of them shared information about how fast they were travelling and how far they were to the nearest airport. “If I arrive earlier to the airport, you will owe me a beer” – the captain bragged, then the airplane started to move abruptly.
-Of course Mr. Ed survived, if not, he could not tell us this story. But weirdly, you are wondering who won the race between the pilots and their average arrival time, so you asked the velocity and distance to the airport of both planes. Assume that the planes maintained their velocity even when landing.
-Input
-The input will contain several test cases. The only line of each test case contains 4 space-separated integers v1, d1 , v2, d2
-(1 <= v1,d1,v2,d2 <= 10^9): the velocity and distance to the airport of the plane Mr. Ed and the captain were and the velocity and distance to the airport of the plane the captain was competing with. Velocities are expressed in miles per hour and distances in miles.
-The last test case is followed by a single line containing 4 zeroes.
+// use std::io;
+// use std::io::Read;
 
-Output
-Print 2 lines for each test case. In the first one, you should print "You owe me a beer!" if the captain won the race or "No beer for the captain." if the other airplane won the race.
-You can safely assume there will be no draws in any test case.
-
-In the second line, print "Avg. arrival time:" followed by the average arrival time (in hours) of both airplanes expressed as a simplified fraction of the form 
-x/y, being  x and y integers. If the fraction has an integer result, print the result of the division. See format below for more details.
-
-Example
-Input:
-2 4 1 3
-1 3 2 4
-4 7 4 9
-0 0 0 0
-
-Output:
-Case #1: You owe me a beer!
-Avg. arrival time: 5/2
-Case #2: No beer for the captain.
-Avg. arrival time: 5/2
-Case #3: You owe me a beer!
-Avg. arrival time: 2
-
-*/
-
-// use std::io::{self};
+// fn gcd(mut a: usize, mut b: usize) -> usize {
+//     while b != 0 {
+//         let temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+//     a
+// }
 
 // fn main() {
-//     let mut input = String::new();
-//     let mut case = 1;
+//     let mut buffer = String::new();
+//     io::stdin().read_to_string(&mut buffer).unwrap();
+//     let input = buffer.lines();
 
-//     loop {
-//         input.clear();
-//         io::stdin().read_line(&mut input).expect("Failed to read input");
-//         let values: Vec<u64> = input
+//     let mut case_number = 1;
+
+//     for line in input {
+//         let numbers: Vec<usize> = line
 //             .split_whitespace()
-//             .map(|s| s.trim().parse().expect("Failed to parse input"))
+//             .map(|x| x.parse().unwrap())
 //             .collect();
 
-//         if values.len() == 4 && values.iter().all(|&x| x == 0) {
+//         if numbers[0] == 0 && numbers[1] == 0 && numbers[2] == 0 && numbers[3] == 0 {
 //             break;
 //         }
 
-//         let v1 = values[0];
-//         let d1 = values[1];
-//         let v2 = values[2];
-//         let d2 = values[3];
-
-//         let time1 = (d1 as f64) / (v1 as f64);
-//         let time2 = (d2 as f64) / (v2 as f64);
-
-//         println!("Case #{}: {}", case, if time1 < time2 { "You owe me a beer!" } else { "No beer for the captain." });
-
-//         let avg_time = (time1 + time2) / 2.0;
-
-//         let avg_time_numerator = (avg_time * 2.0) as u64;
-//         let avg_time_denominator = 2;
-
-//         if avg_time_denominator == 1 {
-//             println!("Avg. arrival time: {}", avg_time_numerator);
+//         let c = numbers[0] * numbers[3];
+//         let o = numbers[1] * numbers[2];
+        
+//         if c > o {
+//             println!("Case #{}: You owe me a beer!", case_number);
 //         } else {
-//             if avg_time_numerator % avg_time_denominator == 0 {
-//                 println!("Avg. arrival time: {}", avg_time_numerator / avg_time_denominator);
-//             } else {
-//                 println!("Avg. arrival time: {}/{}", avg_time_numerator, avg_time_denominator);
-//             }
+//             println!("Case #{}: No beer for the captain.", case_number);
 //         }
 
-//         case += 1;
+//         let num = (numbers[0] * numbers[3]) + (numbers[1] * numbers[2]);
+//         let den = numbers[0] * numbers[2];
+//         let p1 = num * 1;
+//         let p2 = den * 2;
+
+
+//         let gcd = gcd(p1, p2);
+//         let num2 = p1 / gcd;
+//         let den2 = p2 / gcd;
+
+//         if den2 == 1{
+//             println!("Avg. arrival time: {}", num2);
+//         } else {
+//             println!("Avg. arrival time: {}/{}", num2, den2);
+//         }
+
+//         case_number += 1;
 //     }
 // }
 
